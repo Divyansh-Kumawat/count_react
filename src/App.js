751fe8527e76b2ee37e90c47d018d45603acac06
Divyansh-Kumawat <divyanshmcp@gmail.com>
2024-11-05 21:51:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import About from './components/About';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+
+
+  return (
+    <> 
+      <Router>
+        <Navbar title="DeeKay" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Switch>
+            <Route exact path="/about">
+              <About/>
+            </Route>
+            <Route exact path="/">
+              <TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+    </>
+  );
+}
+
+export default App;
